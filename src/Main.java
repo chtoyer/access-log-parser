2ddb58ec401cb7d1bbcdc1fde3fd00b4bd49f524
Anna Tkacheva <annatkacheva02@mail.ru>
2026-03-13 17:13:06 +0300

    Задание #1. Обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int number1 = new Scanner(System.in) .nextInt();
-        System.out.println("Введите второе число:");
-        int number2 = new Scanner(System.in) .nextInt();
+        System.out.println("Введите путь к файлу:");
+        String path = new Scanner(System.in).nextLine();
+        File file = new File(path);
 
-        int sum = number1 + number2;
-        int diff = number1 - number2;
-        int product = number1 * number2;
-        double quotient = (double) number1 / number2;
+        if (!file.exists() || file.isDirectory()) {
+            System.out.println("Указанный путь не является файлом или не существует.");
+            return;
+        }
 
-        System.out.println("Сумма: " + sum);
-        System.out.println("Разность: " + diff);
-        System.out.println("Произведение: " + product);
-        if (number2 != 0) {System.out.println("Частное: " + quotient);}
-        else System.out.println("Частное: Не определено");
+        int totalLines = 0;
+        int maxLength = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int length = line.length();
+
+                if (length > 1024) {
+                    throw new LineTooLongException("Строка слишком длинная: " + length + " символов");
+                }
+                totalLines++;
+                if (length > maxLength) maxLength = length;
+                if (length < minLength) minLength = length;
+            }
+
+            if (totalLines == 0) minLength = 0;
+
+            System.out.println("Общее количество строк в файле: " + totalLines);
+            System.out.println("Длина самой длинной строки: " + maxLength);
+            System.out.println("Длина самой короткой строки: " + minLength);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
